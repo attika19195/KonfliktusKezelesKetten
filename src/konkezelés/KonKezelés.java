f2b3c625d95f6449b99350819efb39387a1245b5
attika19195 <attika19195@gmail.com>
2022-03-10 13:17:16 +0100

    ,ásodik név inicializálása

--- src/konkezelés/KonKezelés.java
+++ src/konkezelés/KonKezelés.java
@@ -2,7 +2,7 @@ package konkezelés;
 
 public class KonKezelés {
     public static void main(String[] args) {
-        String nev1;
+        String nev1 = "Polgár Attila";
         String nev2 = "Vitay Zalán";
     }
 }
