4d0f7f8904646773431dcb3725590c105bd549fa
ForGetASD <vitayz60@gmail.com>
2022-03-10 13:05:44 +0100

    saját név

--- src/konkezelés/KonKezelés.java
+++ src/konkezelés/KonKezelés.java
@@ -2,6 +2,7 @@ package konkezelés;
 
 public class KonKezelés {
     public static void main(String[] args) {
-        String nev1,nev2;
+        String nev1;
+        String nev2 = "Vitay Zalán";
     }
 }
